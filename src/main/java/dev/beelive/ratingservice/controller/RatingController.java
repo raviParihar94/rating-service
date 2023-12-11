9c9428685400de9fec9f4168298d4ab5f26190cd
raviParihar94 <pariharravi480@gmail.com>
2023-12-11 23:55:48 +0530

    Initial commit

---
+++ src/main/java/dev/beelive/ratingservice/controller/RatingController.java
@@ -0,0 +1,12 @@
+package dev.beelive.ratingservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rating")
+public class RatingController {
+
+
+
+}
