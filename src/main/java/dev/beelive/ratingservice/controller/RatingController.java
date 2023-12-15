e8a419dff25165156a18a0bcd17c941865ff1dd0
raviParihar94 <pariharravi480@gmail.com>
2023-12-15 08:30:23 +0530

    Creating Rating Service APIs

--- src/main/java/dev/beelive/ratingservice/controller/RatingController.java
+++ src/main/java/dev/beelive/ratingservice/controller/RatingController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratings")
 public class RatingController {
@@ -19,5 +21,20 @@ public class RatingController {
         return  ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
     }
 
+    @GetMapping("/all-ratings")
+    public ResponseEntity<List<Rating>> getAllRatings(){
 
+        return ResponseEntity.ok(ratingService.getAllRatings());
+    }
+    @GetMapping("/user/{userID}")
+    public ResponseEntity<List<Rating>> getAllRatingsByUserID(@PathVariable String userID){
+
+        return ResponseEntity.ok(ratingService.getRatingByUserID(userID));
+    }
+    @GetMapping("/hotels/{hotelID}")
+    public ResponseEntity<List<Rating>> getAllRatingsByHotelID(@PathVariable  String hotelID){
+
+        return ResponseEntity.ok(ratingService.getRatingByHotelID(hotelID));
+    }
 }
+
